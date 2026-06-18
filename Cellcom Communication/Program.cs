01e59e37629f066717f2a3cdba310bdc7bb95814
Ilya Beg <ilyabegichev@gmail.com>
2026-06-18 18:47:44 +0300

    Add project files.

---
+++ Cellcom Communication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cellcom_Communication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
