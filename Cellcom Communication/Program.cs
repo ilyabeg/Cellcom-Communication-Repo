37df615c71d9fb94df902cb784c69c1715b4485b
Ilya Beg <ilyabegichev@gmail.com>
2026-06-20 15:27:34 +0300

    commit #1 starting point (rs232 configuration and SerialPort namespace link)

--- Cellcom Communication/Program.cs
+++ Cellcom Communication/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Cellcom_Communication
+﻿using System;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Cellcom_Communication
 {
     internal class Program
     {
+        static SerialPort serialPort = new SerialPort(); // global serial port
+
+        public class Customer
+        { 
+            // each customer has 3 cellcom methods...
+            public void JoinCellcom()
+            {
+            }
+
+            public void InitiateCall()
+            {
+            }
+
+            public void CloseCall() 
+            {
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // RS-232 protocol configuration:
+            serialPort.BaudRate = 9600;
+            serialPort.DataBits = 8;
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+
         }
     }
 }
