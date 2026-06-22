c98347aaa5eeae5b1762a74020cc47a43b227e85
Ilya Beg <ilyabegichev@gmail.com>
2026-06-22 16:24:27 +0300

    commit #3 small changes (server and client currently flipped!)

--- Cellcom Communication/Program.cs
+++ Cellcom Communication/Program.cs
@@ -11,19 +11,38 @@ namespace Cellcom_Communication
 
         public class Customer
         {
-            public string ID;
+            public string ID { get; set; } = "unknown.user"; // default value for ID
+            private bool _callActive;
 
             // each customer has 3 cellcom methods...
-            public void JoinCellcom()
+            public void JoinCellcom(string command)
             {
+                serialPort.WriteLine(String.Format("<{0}> : {1} : JOINING CELLCOM ENTERPRISE...", this.ID, command));
+                for (int i = 1; i <= 10; i++)
+                {
+                    serialPort.Write(i + " ");
+                    Thread.Sleep(500); // simulate some work being done...
+                }
+                serialPort.WriteLine(String.Format("\r\n<{0}> : DONE", this.ID));
             }
 
-            public void InitiateCall()
+            public async Task InitiateCall(string command)
             {
+                serialPort.WriteLine(String.Format("<{0}> : {1}", this.ID, command));
+                _callActive = true; // activate call
+
+                while (_callActive)
+                {
+                    serialPort.WriteLine(String.Format("<{0}> : CELLCOM", this.ID));
+                    await Task.Delay(1000); // delay one second...
+                }
             }
 
-            public void CloseCall() 
+            public void CloseCall(string command)
             {
+                serialPort.WriteLine(String.Format("<{0}> : {1}", this.ID, command));
+                serialPort.WriteLine(String.Format("<{0}> : BYE.", this.ID));
+                _callActive = false; // close call
             }
         }
 
@@ -31,7 +50,6 @@ namespace Cellcom_Communication
         {
             serialPort = new SerialPort();
             Thread commandsThread = new Thread(ReadCommands);
-            string tmpID;
 
             // list available serial com ports on pc...
             Console.WriteLine("Available Ports:");
@@ -47,22 +65,17 @@ namespace Cellcom_Communication
             serialPort.Parity = Parity.None;
             serialPort.StopBits = StopBits.One;
             serialPort.Handshake = Handshake.None;
+            serialPort.NewLine = "\r\n"; // forces return upon a new line.
 
             // read/write timeouts in milliseconds:
             serialPort.ReadTimeout = 500;
             serialPort.WriteTimeout = 500;
 
+            // creating new customer and setting ID
             Customer c1 = new Customer();
-            Console.Write("Enter customer ID -> ");
-            tmpID = Console.ReadLine();
+            c1.ID = SetCustomerID(c1.ID);
 
-            while (tmpID == null || tmpID == "")
-            {
-                Console.Write("\nInvalid input. Please re-enter ID -> ");
-                tmpID = Console.ReadLine();
-            }
-            c1.ID = tmpID;
-
+            // open serial port and start commands thread...
             serialPort.Open();
             _continue = true;
             commandsThread.Start();
@@ -74,15 +87,26 @@ namespace Cellcom_Communication
             {
                 command = Console.ReadLine();
 
-                if (command.Equals("EXIT") || command.Equals("exit"))
+                switch (command)
                 {
-                    _continue = false;
-                } else
-                {
-                    serialPort.WriteLine(String.Format("<{0}>: {1}", c1.ID, command));
+                    case "EXIT" or "Exit" or "exit":
+                        _continue = false;
+                        break;
+                    case "JOIN" or "Join" or "join":
+                        c1.JoinCellcom(command);
+                        break;
+                    case "NEW" or "New" or "new":
+                        c1.InitiateCall(command);
+                        break;
+                    case "STOP" or "Stop" or "stop":
+                        c1.CloseCall(command);
+                        break;
+                    default:
+                        serialPort.WriteLine(String.Format("<{0}> : {1} : Command not found.", c1.ID, command));
+                        break;
                 }
             }
-            
+
             commandsThread.Join();
             serialPort.Close();
         }
@@ -93,12 +117,29 @@ namespace Cellcom_Communication
             {
                 try
                 {
-
+                    //string command = serialPort.ReadLine();
+                    //Console.WriteLine(command);
                 }
-                catch (TimeoutException e) 
-                { 
+                catch (TimeoutException e)
+                {
                     Console.WriteLine("\n\tError. Application stopped due to timeout exception: " + e.Message + ".");
                 }
+            }
+        }
+
+        public static string SetCustomerID(string defaultID)
+        {
+            Console.WriteLine("Enter customer ID (or press Enter to use default ID: {0}):", defaultID);
+            string inputID = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(inputID))
+            {
+                return defaultID;
+            }
+            else
+            {
+                Console.WriteLine("\tCustomer ID set to: {0}", inputID);
+                return inputID;
             }
         }
     }
