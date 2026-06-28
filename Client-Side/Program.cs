0039c20e01b32eeeb9609067edeb780af4fe2f36
Ilya Beg <ilyabegichev@gmail.com>
2026-06-28 13:37:11 +0300

    feat: Add 10 client limit

--- Client-Side/Program.cs
+++ Client-Side/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Client_Side
+﻿using Client_Side;
+using System.Diagnostics;
+
+namespace Client_Side
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var currentName = Process.GetCurrentProcess().ProcessName; // השם של התכנית הנוכחית
+            Process[] currentRunningInstances = Process.GetProcessesByName(currentName); // מחזיר מערך של כמות התכניות שכרגע רצות - כלומר המשתמשים
+            
+            // limit 10 users
+            if (currentRunningInstances.Length > 10)
+            {
+                Console.WriteLine("Max user limit reached.");
+                return;
+            }
+
+            // run program if ok...
             Client client = new Client();
             client.Start();
         }
