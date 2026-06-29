6e1ec014fd385e5010486347bae9e3f32b955ac7
Ilya Beg <ilyabegichev@gmail.com>
2026-06-29 15:01:31 +0300

    feat: Add list of available ports to client

--- Client-Side/Client.cs
+++ Client-Side/Client.cs
@@ -13,7 +13,8 @@ namespace Client_Side
         {
             InitPorts();
 
-            Console.WriteLine("Select COM port (e.g. COM10, COM11, ... , COM19) >>");
+            Console.WriteLine("Select COM port >>");
+            DisplayAvailablePorts();
             string input = Console.ReadLine().Trim().ToUpper();
 
             if (!isValid(input))
@@ -29,7 +30,12 @@ namespace Client_Side
             SerialPort openPort = OpenPort(input);
             if (openPort == null)
             {
-                Console.WriteLine("Couldn't find/open port. Exiting program...");
+                Console.WriteLine("Couldn't find/open port. Exiting program in 5 seconds...");
+                for (int i = 5; i >= 1; i--)
+                {
+                    Console.Write(i + " ");
+                    Thread.Sleep(1000);
+                }
                 return;
             }
 
@@ -39,11 +45,10 @@ namespace Client_Side
         private void InputClientCommands(SerialPort serialPort)
         {
             string command;
-            bool _continue = true;
 
             Console.WriteLine("Type QUIT to close the program...\n");
 
-            while (_continue)
+            while (true)
             {
                 command = Console.ReadLine();
 
@@ -145,6 +150,27 @@ namespace Client_Side
                 }
             }
             return false;
+        }
+
+        private void DisplayAvailablePorts()
+        {
+            Console.WriteLine("Available COM ports: ");
+            foreach (SerialPort port in availablePorts)
+            {
+                if (!port.IsOpen)
+                {
+                    try
+                    {
+                        port.Open();
+                        port.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine("\t- " + port.PortName);
+                }
+            }
         }
     }
 }
